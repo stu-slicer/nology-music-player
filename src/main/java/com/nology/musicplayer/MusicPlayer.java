7a124b5f0b2471a47d018242cbffba277181696a
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-03-14 16:03:33 +0000

    Simplify the building of player - remove individual components.

--- src/main/java/com/nology/musicplayer/MusicPlayer.java
+++ src/main/java/com/nology/musicplayer/MusicPlayer.java
@@ -4,12 +4,8 @@ import com.nology.musicplayer.controller.MusicController;
 import com.nology.musicplayer.database.DBUtils;
 import com.nology.musicplayer.database.DatabaseInitialiser;
 import com.nology.musicplayer.database.JdbcTrackService;
-import com.nology.musicplayer.frontend.ConsoleTrackDisplayer;
 import com.nology.musicplayer.frontend.TrackDisplayer;
-import com.nology.musicplayer.player.TextTrackPlayer;
 import com.nology.musicplayer.player.TrackPlayer;
-
-import javax.naming.OperationNotSupportedException;
 
 public class MusicPlayer {
 
@@ -22,48 +18,24 @@ public class MusicPlayer {
     }
 
     private void buildAndStart() {
-        DBUtils dbUtils = buildDBUtils();
+        DBUtils dbUtils = new DBUtils();
+
+        // build DatabaseInitialiser
+
+        // initialise DB
+
+        // build JdbcTrackService
+
+        // build a TrackPlayer
+
+        // build a TrackDisplayer
+
+        // finally. build the MusicController
+
 
         // initialise the controller
         //controller.initialiseController();
 
-    }
-
-    // BUILD MusicController
-    private MusicController buildMusicController(DBUtils dbUtils) {
-        throw new UnsupportedOperationException();
-    }
-
-    // BUILD TrackDisplayer
-    private TrackDisplayer buildTrackDisplayer(RendererType type) {
-        throw new UnsupportedOperationException();
-    }
-
-    // BUILD TrackPlayer
-    private TrackPlayer buildTrackPlayer(PlayerType type) {
-        throw new UnsupportedOperationException();
-    }
-
-    // BUILD JdbcTrackService
-    private JdbcTrackService buildJdbcTrackService(DBUtils dbUtils) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Builds and initialises the {@link DatabaseInitialiser} object.
-     * @param dbUtils
-     * @return
-     */
-    private DatabaseInitialiser buildDatabaseInitialiser(DBUtils dbUtils) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Builds and initalises the {@link DBUtils} class.
-     * @return
-     */
-    private DBUtils buildDBUtils() {
-        return new DBUtils();
     }
 
     public static void main(String[] args) {
