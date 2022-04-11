f243316c17463559d89d991ca26245d0952a2982
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-04-11 17:40:54 +0100

    Add in code to build the player.

--- src/main/java/com/nology/musicplayer/MusicPlayer.java
+++ src/main/java/com/nology/musicplayer/MusicPlayer.java
@@ -4,7 +4,9 @@ import com.nology.musicplayer.controller.MusicController;
 import com.nology.musicplayer.database.DBUtils;
 import com.nology.musicplayer.database.DatabaseInitialiser;
 import com.nology.musicplayer.database.JdbcTrackService;
+import com.nology.musicplayer.frontend.ConsoleTrackDisplayer;
 import com.nology.musicplayer.frontend.TrackDisplayer;
+import com.nology.musicplayer.player.TextTrackPlayer;
 import com.nology.musicplayer.player.TrackPlayer;
 
 public class MusicPlayer {
@@ -21,21 +23,29 @@ public class MusicPlayer {
         DBUtils dbUtils = new DBUtils();
 
         // build DatabaseInitialiser
+        DatabaseInitialiser databaseInitialiser = new DatabaseInitialiser(dbUtils);
 
         // initialise DB
+        databaseInitialiser.initialiseDatabase();
 
         // build JdbcTrackService
+        JdbcTrackService trackService = new JdbcTrackService(dbUtils);
 
         // build a TrackPlayer
+        TextTrackPlayer trackPlayer = new TextTrackPlayer();
 
         // build a TrackDisplayer
+        ConsoleTrackDisplayer trackDisplayer = new ConsoleTrackDisplayer();
 
         // finally. build the MusicController
-
+        MusicController controller = new MusicController();
+        controller.setTrackPlayer(trackPlayer);
+        controller.setTrackDisplayer(trackDisplayer);
+        controller.setTrackService(trackService);
 
         // initialise the controller
-        //controller.initialiseController();
-
+        controller.initialiseController();
+        controller.run();
     }
 
     public static void main(String[] args) {
